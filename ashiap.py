2d548d0aee4a08428c9042844654c4f51dbde937
ZAK ID Channel <62635702+zakid000@users.noreply.github.com>
2020-03-24 22:11:02 -0700

    Add files via upload

---
+++ ashiap.py
@@ -0,0 +1,78 @@
+#############################
+# A Password List Generator #
+# Copyright ZAK ID  -  2020 #
+# Work with PYTHON 3        #
+#############################
+
+import os
+import time
+
+def mulai():
+    os.system("apt install figlet")
+    os.system("clear")
+    os.system("figlet Ashiap")
+    print("Debian and Ubuntu Based OS ONLY!")
+
+def ulang():
+    mulai()
+    proses()
+
+def proses():
+    try:
+        fname = input("\nroot@Generat0r>> File Name : ")
+        file=open(""+fname+".txt",'w')
+        a = input("\nroot@Generat0r>> Poss Name1 : ")
+        b = input("\nroot@Generat0r>> Poss Name2 : ")
+        c = input("\nroot@Generat0r>> Poss Name3 : ")
+        d = input("\nroot@Generat0r>> Poss Name4 : ")
+        e = input("\nroot@Generat0r>> Poss Name5 : ")
+        f = input("\nroot@Generat0r>> Poss Number1 : ")
+        g = input("\nroot@Generat0r>> Poss Number2 : ")
+        h = input("\nroot@Generat0r>> Poss Number3 : ")
+        i = input("\nroot@Generat0r>> Poss Date1(dd/mm/yy) : " )
+        j = input("\nroot@Generat0r>> Poss Date2(dd/mm/yy) : " )
+        k = input("\nroot@Generat0r>> Poss Date3(dd/mm/yy) : " )
+        i1 = i[0:2]
+        i2 = i[2:4]
+        i3 = i[4:]
+        j1 = j[0:2]
+        j2 = j[2:4]
+        j3 = j[4:]
+        k1 = k[0:2]
+        k2 = k[2:4]
+        k3 = k[4:]
+        file.write("\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s%s%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n" %(a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3,a,b,c,d,e,f,g,h,i,j,k,i1,i2,i3,j1,j2,j3,k1,k2,k3))
+        
+        qq = 0
+        while (qq < 1000):
+            qq = qq + 1
+            file.write(a + str(qq) + '\n')
+            file.write(b + str(qq) + '\n')
+            file.write(c + str(qq) + '\n')
+            file.write(d + str(qq) + '\n')
+            file.write(e + str(qq) + '\n')
+            file.write(a + a + str(qq) + '\n')
+            file.write(b + b + str(qq) + '\n')
+            file.write(c + c + str(qq) + '\n')
+            file.write(d + d + str(qq) + '\n')
+            file.write(e + e + str(qq) + '\n')
+            file.write(a + b + str(qq) + '\n')
+            file.write(b + c + str(qq) + '\n')
+            file.write(c + d + str(qq) + '\n')
+            file.write(d + e + str(qq) + '\n')
+            file.write(e + a + str(qq) + '\n')
+        
+        file.close()
+        time.sleep(1.5)
+        print("BERHASIL")
+        
+    except IOError:
+        print("ERROR")
+        print("ctrl - z to exit")
+        time.sleep(1.5)
+        ulang()
+        
+
+
+mulai()
+proses()
